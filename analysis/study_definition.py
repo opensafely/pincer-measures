046836bc42ffe826535b4f7509ba5a876c78393d
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-08-11 18:22:48 +0100

    Add indicators AC and ME

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -347,16 +347,16 @@ study = StudyDefinition(
         returning="binary_flag",
         include_date_of_match=True,
         date_format="YYYY-MM-DD",
-        on_or_before=["index_date - 15 months"],
+        on_or_before="index_date - 15 months",
     ),
 
     loop_diuretic = patients.with_these_medications(
-        codelist = loop_diuretic_codelist, 
+        codelist = loop_diuretics_codelist, 
         find_first_match_in_period=True,
         returning="binary_flag",
         include_date_of_match=True,
         date_format="YYYY-MM-DD",
-        on_or_before=["index_date - 15 months"],
+        on_or_before="index_date - 15 months",
     ),
 
     acei_recent = patients.with_these_medications(
@@ -369,13 +369,16 @@ study = StudyDefinition(
     ),
 
     loop_diuretic_recent = patients.with_these_medications(
-        codelist = loop_diuretic_codelist, 
+        codelist = loop_diuretics_codelist, 
         find_last_match_in_period=True,
+        returning="binary_flag",
+        include_date_of_match=True,
+        date_format="YYYY-MM-DD",
         between=["index_date - 6 months", "index_date"],
     ),
 
     renal_function_test = patients.with_these_clinical_events(
-        codelist = renal_function_test_codelist,
+        codelist = renal_function_codelist,
         find_last_match_in_period = True,
         returning="binary_flag",
         include_date_of_match=True,
@@ -403,12 +406,76 @@ study = StudyDefinition(
     indicator_ac_numerator = patients.satisfying(
         """
         (age >=75 AND age <=120) AND
-        (loop_diuretic AND loop_diuretic_recent) OR
-        (acei AND acei_recent) AND
+        ((loop_diuretic AND loop_diuretic_recent) OR (acei AND acei_recent))AND
         ((NOT renal_function_test) OR (NOT electrolytes_test))
         """,
     ),
 
+    ###
+    # MONITORING COMPOSITE INDICATOR
+    # ME - Methotrexate audit (MO_P15)
+    ####
+
+    methotrexate_6_3_months = patients.with_these_medications(
+        codelist = methotrexate_codelist, 
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        include_date_of_match=True,
+        date_format="YYYY-MM-DD",
+        between=["index_date - 6 months", "index_date - 3 months"],
+    ),
+
+    methotrexate_3_months = patients.with_these_medications(
+        codelist = methotrexate_codelist, 
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        include_date_of_match=True,
+        date_format="YYYY-MM-DD",
+        between=["index_date - 3 months", "index_date"],
+    ),
+
+    full_blood_count = patients.with_these_clinical_events(
+        codelist = full_blood_count_codelist,
+        find_last_match_in_period = True,
+        returning="binary_flag",
+        include_date_of_match=True,
+        date_format="YYYY-MM-DD",
+        between = ["index_date - 3 months", "index_date"],
+    ),
+
+    liver_function_test = patients.with_these_clinical_events(
+        codelist = liver_function_test_codelist,
+        find_last_match_in_period = True,
+        returning="binary_flag",
+        include_date_of_match=True,
+        date_format="YYYY-MM-DD",
+        between = ["index_date - 3 months", "index_date"],
+    ),
+
+    indicator_me_denominator = patients.satisfying(
+        """
+        methotrexate_6_3_months AND
+        methotrexate_3_months
+        """,
+    ),
+
+    indicator_me_no_fbc_numerator = patients.satisfying(
+        """
+        methotrexate_6_3_months AND
+        methotrexate_3_months AND
+        (NOT full_blood_count)
+        """,
+    ),
+
+    indicator_me_no_lft_numerator = patients.satisfying(
+        """
+        methotrexate_6_3_months AND
+        methotrexate_3_months AND
+        (NOT liver_function_test)
+        """,
+    ),
+
+    
     
 )
 

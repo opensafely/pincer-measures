f19be5bef0e6c96b49fade6e23b8683dd91cfb2e
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-08-12 12:18:32 +0100

    Add function to get date of input file

--- analysis/utilities.py
+++ analysis/utilities.py
@@ -4,3 +4,13 @@ def match_input_files(file: str) -> bool:
     """Checks if file name has format outputted by cohort extractor"""
     pattern = r'^input_20\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])\.csv' 
     return True if re.match(pattern, file) else False
+
+def get_date_input_file(file: str) -> str:
+    """Gets the date in format YYYY-MM-DD from input file name string"""
+    #check format
+    if not match_input_files(file):
+        raise Exception('Not valid input file format')
+    
+    else:
+        date = result = re.search(r'input_(.*)\.csv', file)
+        return date.group(1)

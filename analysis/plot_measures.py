59c41e789b86bb2dd9c5944d2e2a462a61611273
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-08-13 10:01:45 +0100

    Produce all demographic plots

--- analysis/plot_measures.py
+++ analysis/plot_measures.py
@@ -1,6 +1,16 @@
-from utilities import plot_measures
+from utilities import plot_measures, OUTPUT_DIR
 import pandas as pd
+from study_definition import indicators_list
+from calculate_measures import demographics
+import re
 
 df = pd.read_csv("output/indicator_measure_a_age_band.csv")
 
-plot_measures(df = df, filename="plot_a_age_band", title="Indicator A by Age Band",  column_to_plot = "rate", y_label = 'Rate per 1000', as_bar=False, category = 'age_band')
+
+for i in indicators_list:
+    for d in demographics:
+        df = pd.read_csv(OUTPUT_DIR / f"indicator_measure_{i}_{d}.csv")
+        plot_measures(df = df, filename=f"plot_{i}_{d}", title=f"Indicator {i} by {d}",  column_to_plot = "rate", y_label = 'Rate per 1000', as_bar=False, category = d)
+
+
+

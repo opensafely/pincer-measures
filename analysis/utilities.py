50622e605f5e471cf637fba52bb6cabf1a4a3808
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-08-13 10:10:00 +0100

    Add decile chart functions

--- analysis/utilities.py
+++ analysis/utilities.py
@@ -145,3 +145,138 @@ def drop_irrelevant_practices(df):
     """
     is_relevant = df.groupby("practice").value.any()
     return df[df.practice.isin(is_relevant[is_relevant == True].index)]
+
+# https://github.com/ebmdatalab/datalab-pandas/blob/master/ebmdatalab/charts.py
+def deciles_chart_ebm(
+    df,
+    period_column=None,
+    column=None,
+    title="",
+    ylabel="",
+    show_outer_percentiles=True,
+    show_legend=True,
+    ax=None,
+):
+    """period_column must be dates / datetimes"""
+    sns.set_style("whitegrid", {"grid.color": ".9"})
+    if not ax:
+        fig, ax = plt.subplots(1, 1)
+    df = compute_deciles(df, period_column, column, show_outer_percentiles)
+    linestyles = {
+        "decile": {
+            "line": "b--",
+            "linewidth": 1,
+            "label": "decile",
+        },
+        "median": {
+            "line": "b-",
+            "linewidth": 1.5,
+            "label": "median",
+        },
+        "percentile": {
+            "line": "b:",
+            "linewidth": 0.8,
+            "label": "1st-9th, 91st-99th percentile",
+        },
+    }
+    label_seen = []
+    for percentile in range(1, 100):  # plot each decile line
+        data = df[df["percentile"] == percentile]
+        add_label = False
+
+        if percentile == 50:
+            style = linestyles["median"]
+            add_label = True
+        elif show_outer_percentiles and (percentile < 10 or percentile > 90):
+            style = linestyles["percentile"]
+            if "percentile" not in label_seen:
+                label_seen.append("percentile")
+                add_label = True
+        else:
+            style = linestyles["decile"]
+            if "decile" not in label_seen:
+                label_seen.append("decile")
+                add_label = True
+        if add_label:
+            label = style["label"]
+        else:
+            label = "_nolegend_"
+
+        ax.plot(
+            data[period_column],
+            data[column],
+            style["line"],
+            linewidth=style["linewidth"],
+            label=label,
+        )
+    ax.set_ylabel(ylabel, size=15, alpha=0.6)
+    if title:
+        ax.set_title(title, size=18)
+    # set ymax across all subplots as largest value across dataset
+    ax.set_ylim([0, df[column].max() * 1.05])
+    ax.tick_params(labelsize=12)
+    ax.set_xlim(
+        [df[period_column].min(), df[period_column].max()]
+    )  # set x axis range as full date range
+
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=90)
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%B %Y"))
+    if show_legend:
+        ax.legend(
+            bbox_to_anchor=(1.1, 0.8),  # arbitrary location in axes
+            #  specified as (x0, y0, w, h)
+            loc=CENTER_LEFT,  # which part of the bounding box should
+            #  be placed at bbox_to_anchor
+            ncol=1,  # number of columns in the legend
+            fontsize=12,
+            borderaxespad=0.0,
+        )  # padding between the axes and legend
+        #  specified in font-size units
+    # rotates and right aligns the x labels, and moves the bottom of the
+    # axes up to make room for them
+    plt.gcf().autofmt_xdate()
+    return plt
+
+
+def compute_deciles(
+    measure_table, groupby_col, values_col, has_outer_percentiles=True
+):
+    """Computes deciles.
+    Args:
+        measure_table: A measure table.
+        groupby_col: The name of the column to group by.
+        values_col: The name of the column for which deciles are computed.
+        has_outer_percentiles: Whether to compute the nine largest and nine smallest
+            percentiles as well as the deciles.
+    Returns:
+        A data frame with `groupby_col`, `values_col`, and `percentile` columns.
+    """
+    quantiles = np.arange(0.1, 1, 0.1)
+    if has_outer_percentiles:
+        quantiles = np.concatenate(
+            [quantiles, np.arange(0.01, 0.1, 0.01), np.arange(0.91, 1, 0.01)]
+        )
+
+    percentiles = (
+        measure_table.groupby(groupby_col)[values_col]
+        .quantile(pd.Series(quantiles, name="percentile"))
+        .reset_index()
+    )
+    percentiles["percentile"] = percentiles["percentile"] * 100
+    return percentiles
+
+
+def deciles_chart(
+    df, period_column=None, column=None, title="", ylabel="", interactive=True
+):
+    """period_column must be dates / datetimes"""
+
+    df = compute_deciles(df, period_column, column, False)
+
+    deciles_chart_ebm(
+        df,
+        period_column="date",
+        column="num_per_thousand",
+        ylabel="rate per 1000",
+        show_outer_percentiles=False,
+    )

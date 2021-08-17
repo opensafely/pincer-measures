6b14783e2d39e089a3e36d6e156dc3348cb22351
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-08-17 14:43:19 +0100

    style: update to file names

--- analysis/check_EGFR.py
+++ analysis/check_EGFR.py
@@ -17,38 +17,31 @@ for file in OUTPUT_DIR.iterdir():
         print(f"Reading EGFR data from {file.stem} ({len(df)})")
         all_egfr_data.append( df[['egfr', 'egfr_less_than_45', 'source']] )
 
+print( f"Output directory is: {OUTPUT_DIR}" )
+
 df = pd.concat(all_egfr_data)
 print( f"Final data frame size: {df.shape}" )
 
 ## All data in one boxplot
 df.boxplot(column='egfr')
-plt.savefig(f"{OUTPUT_DIR}/EFGR_boxplot_ALL.png", format="png")
+plt.savefig(f"{OUTPUT_DIR}/BOXPLOT_EGFRvalue.png", format="png")
 
-## egfr_less_than_45 results don't look quite right...?
-print(df.head(10))
+## Data separated by the less than 45 flag
 df.boxplot(column='egfr', by=['egfr_less_than_45'])
-plt.savefig(f"{OUTPUT_DIR}/EFGR_boxplot_ALL+flagged.png", format="png")
+plt.savefig(f"{OUTPUT_DIR}/BOXPLOT_EGFRvalue-by-EGFR45flag.png", format="png")
 
-## Trying with seaborn - THIS TAKES A LONG TIME TO RUN APPARENTLY!
-# https://stackoverflow.com/questions/29779079/adding-a-scatter-of-points-to-a-boxplot-using-matplotlib
-# sns.set(style="whitegrid")
-# ax = sns.boxplot(x="egfr_less_than_45", y="egfr", data=df, showfliers=False)
-# ax = sns.swarmplot(x="egfr_less_than_45", y="egfr", data=df, color=".25")
-# fig = sns_plot.get_figure()
-# fig.savefig(f"{OUTPUT_DIR}/EFGR_boxplot_ALL+flagged_seaborn.png")
-
-## Using groupby() to group by input file source (i.e., date)
+## Data separated using groupby() to group by input file source (i.e., date)
 df.boxplot( column='egfr', by='source' )
-plt.savefig(f"{OUTPUT_DIR}/EFGR_boxplot_bysource.png", format="png")
+plt.savefig(f"{OUTPUT_DIR}/BOXPLOT_EGFRvalue-by-month.png", format="png")
 
 ## crosstabs
 # https://towardsdatascience.com/meet-the-hardest-functions-of-pandas-part-ii-f8029a2b0c9b
 EGFR_less_than_45_crosstabs = pd.crosstab( index=df['source'], columns=df['egfr_less_than_45'] )
-print(EGFR_less_than_45_crosstabs)
+EGFR_less_than_45_crosstabs.to_csv( f"{OUTPUT_DIR}/CROSSTAB_source-by-EGFR45flag.csv" )
 
 ## Counting missing values
 df['missing_egfr'] = pd.isnull(df['egfr'])
-print(df.head(10))
 EGFR_missing_crosstabs = pd.crosstab(index=df['source'], columns=df['missing_egfr'])
-print(EGFR_missing_crosstabs)
+EGFR_missing_crosstabs.to_csv(f"{OUTPUT_DIR}/CROSSTAB_source-by-missingEGFR.csv")
+
 

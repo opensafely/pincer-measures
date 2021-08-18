3ea636549486a0103887309269ecab6c5ac018d6
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-08-18 13:28:54 +0100

    test: removing unnecessary tests from SRO-measures

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -119,31 +119,6 @@ def measure_table():
 #     )
 
 
-# class TestLoadAndDrop:
-#     def test_practice_is_false(self, tmp_path, measure_table_from_csv):
-#         # What's going on here, then? We are patching, or temporarily
-#         # replacing, `utilities.OUTPUT_DIR` with `tmp_path`, which is
-#         # a pytest fixture that returns a temporary directory as
-#         # a `pathlib.Path` object.
-#         with patch.object(utilities, "OUTPUT_DIR", tmp_path):
-#             measure = "systolic_bp"
-#             f_name = f"measure_{measure}.csv"
-#             measure_table_from_csv.to_csv(utilities.OUTPUT_DIR / f_name)
-
-#             obs = utilities.load_and_drop(measure)
-#             assert is_datetime64_dtype(obs.date)
-#             assert all(obs.practice.values == [2, 3, 2])
-
-#     def test_practice_is_true(self, tmp_path, measure_table_from_csv):
-#         with patch.object(utilities, "OUTPUT_DIR", tmp_path):
-#             measure = "systolic_bp"
-#             f_name = f"measure_{measure}_practice_only.csv"
-#             measure_table_from_csv.to_csv(utilities.OUTPUT_DIR / f_name)
-
-#             obs = utilities.load_and_drop(measure, practice=True)
-#             assert is_datetime64_dtype(obs.date)
-#             assert all(obs.practice.values == [2, 3, 2])
-
 
 def test_calculate_rate():
     mt = pandas.DataFrame(
@@ -183,66 +158,6 @@ class TestDropIrrelevantPractices:
         obs = utilities.drop_irrelevant_practices(measure_table_from_csv)
         assert id(obs) != id(measure_table_from_csv)
 
-
-# def test_create_child_table(measure_table, codelist_table_from_csv):
-#     obs = utilities.create_child_table(
-#         measure_table,
-#         codelist_table_from_csv,
-#         "code",
-#         "term",
-#         "systolic_bp",
-#     )
-#     exp = pandas.DataFrame(
-#         [
-#             {
-#                 "code": 1,
-#                 "Events": 2,
-#                 "Events (thousands)": 0.002,
-#                 "Description": "Code 1",
-#             },
-#             {
-#                 "code": 2,
-#                 "Events": 1,
-#                 "Events (thousands)": 0.001,
-#                 "Description": "Code 2",
-#             },
-#         ],
-#     )
-#     testing.assert_frame_equal(obs, exp)
-
-
-# def test_get_number_practices(measure_table):
-#     assert utilities.get_number_practices(measure_table) == 2
-
-
-# def test_get_percentage_practices(tmp_path, measure_table):
-#     with patch.object(utilities, "OUTPUT_DIR", tmp_path):
-#         with open(utilities.OUTPUT_DIR / "practice_count.json", "w") as f:
-#             json.dump({"num_practices": 2}, f)
-
-#         obs = utilities.get_percentage_practices(measure_table)
-#         assert obs == 100
-
-
-# def test_get_number_events_mil(measure_table):
-#     obs = utilities.get_number_events_mil(
-#         measure_table,
-#         "systolic_bp",
-#     )
-#     assert obs == 0.0
-
-
-# def test_get_number_patients(tmp_path):
-#     measure = "systolic_bp"
-
-#     with patch.object(utilities, "OUTPUT_DIR", tmp_path):
-#         with open(utilities.OUTPUT_DIR / "patient_count.json", "w") as f:
-#             json.dump({"num_patients": {measure: 3}}, f)
-
-#         obs = utilities.get_number_patients(measure)
-#         assert obs == 3
-
-
 @pytest.mark.parametrize(
     "has_outer_percentiles,num_rows",
     [
@@ -268,47 +183,3 @@ def test_compute_deciles(measure_table, has_outer_percentiles, num_rows):
     assert is_datetime64_dtype(obs.date)
     assert is_numeric_dtype(obs.percentile)
     assert is_numeric_dtype(obs.value)
-
-
-# class TestGenerateSentinelMeasure:
-#     def test_print_to_stdout(
-#         self,
-#         capsys,  # pytest fixture that captures output stdout and stderr
-#         measure_table,
-#         codelist_table_from_csv,
-#     ):
-#         measure = "systolic_bp"
-#         data_dict = {measure: measure_table}
-#         data_dict_practice = {measure: measure_table}
-#         codelist_dict = {measure: codelist_table_from_csv}
-#         code_column = "code"
-#         term_column = "term"
-#         dates_list = None
-#         interactive = False
-
-#         # It's easier to patch each function that returns a value
-#         # that's printed to stdout than it is to patch each file
-#         # upon which each function depends.
-#         with patch(
-#             "notebooks.utilities.get_number_practices", return_value=1
-#         ), patch(
-#             "notebooks.utilities.get_percentage_practices", return_value=100
-#         ), patch(
-#             "notebooks.utilities.get_number_events_mil", return_value=0.0
-#         ), patch(
-#             "notebooks.utilities.get_number_patients", return_value=1
-#         ):
-#             utilities.generate_sentinel_measure(
-#                 data_dict,
-#                 data_dict_practice,
-#                 codelist_dict,
-#                 measure,
-#                 code_column,
-#                 term_column,
-#                 dates_list,
-#                 interactive,
-#             )
-#             captured = capsys.readouterr()
-#             assert captured.out.startswith(
-#                 "Practices included: 1 (100%)\nTotal patients: 1.00M (0.00M events)\n"
-#             )

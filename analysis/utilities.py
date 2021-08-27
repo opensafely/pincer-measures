5dfeb8f7c02cdd8152ad9b132ae15d0e655cf3ff
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-08-27 14:34:48 +0100

    check egfr one month only

--- analysis/utilities.py
+++ analysis/utilities.py
@@ -31,7 +31,7 @@ def match_input_files(file: str) -> bool:
 
 def match_egfr_files( file: str ) -> bool:
     """Checks if file name has format outputted by cohort extractor (EGFR values)"""
-    pattern = r'^input_egfr_20\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])\.csv'
+    pattern = r'^input_egfr_2021-07-01.csv'
     return True if re.match(pattern, file) else False
 
 def get_date_input_file(file: str) -> str:

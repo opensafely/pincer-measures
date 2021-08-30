73aa83ff89d82648d537fa9ad554817341845e17
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-08-30 19:33:12 +0100

    add died flag

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -22,6 +22,7 @@ study = StudyDefinition(
     population=patients.satisfying(
        """
        registered AND
+       NOT died AND
        (age >=18 AND age <=120) AND
        (
            (age >=65 AND ppi) OR
@@ -41,6 +42,12 @@ study = StudyDefinition(
     ),
 
     registered = patients.registered_as_of("index_date"),
+
+    died = patients.died_from_any_cause(
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence": 0.1}
+        ),
 
     practice=patients.registered_practice_as_of(
         "index_date",

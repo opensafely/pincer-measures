c95b215b2eed15c9dbf91a56c1e642e38ad7df5a
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-09-06 09:13:38 +0100

    test: added for check_input_file_exists()

--- analysis/tests/testthat/test_input_data_handling.R
+++ analysis/tests/testthat/test_input_data_handling.R
@@ -22,14 +22,38 @@ test_that( "Column names are amended correctly",
                   test_m@expected_columns %>% names %>% sort )
 )
 
-test_that( "All missing column names are identified", {
-    expect_equal( sort( identify_missing_column_names(
-        test_m@expected_columns %>% names,
-        test_df) ),
-        test_m@expected_columns %>% names %>% sort
-    )
+test_that( "All missing column names are identified",
+           expect_equal( sort( identify_missing_column_names(
+               test_m@expected_columns %>% names,
+               test_df) ),
+               test_m@expected_columns %>% names %>% sort
+           )
+)
+
+test_that( "Input file check works", {
+    t = tempfile(pattern = "file", tmpdir = tempdir(), fileext = "")
+    test_m@csv_name = basename( t )
+    test_m@name = basename( t )
+    test_m@indir = dirname( t )
+    write_csv( test_df, t )
+    
+    expect_equal( check_input_file_exists(test_m),
+                  glue( "{default_values@outdir}/{basename(t)}" ) )
+    
 })
 
+test_that( "Missing input file gives error", {
+    t = tempfile(pattern = "file", tmpdir = tempdir(), fileext = "")
+    test_m@csv_name = basename( t )
+    test_m@name = basename( t )
+    test_m@indir = dirname( t )
+    write_csv( test_df, t )
+    
+    system( glue( "rm {t}" ) )
+    
+    expect_error( check_input_file_exists(test_m) )
+    
+})
 
 
 

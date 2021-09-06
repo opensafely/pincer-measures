2d1ed5bf69247fc21103c8e3a73a5bb132dc80c4
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-09-06 10:20:50 +0100

    test: added for date_to_epoch() plus tidying

--- analysis/tests/testthat/test_input_data_handling.R
+++ analysis/tests/testthat/test_input_data_handling.R
@@ -1,4 +1,3 @@
-context( "Input data handling")
 
 n = 4
 
@@ -6,8 +5,8 @@ test_df = data.frame(
     object = LETTERS[1:n],
     a_numerator = 1:n,
     a_denominator = (1:n)+2,
-    timestamp = sample(seq(as.Date('1999/01/01'),
-                           as.Date('2000/01/01'), by="day"), n)
+    timestamp = ymd(sample(seq(as.Date('1999/01/01'),
+                           as.Date('2000/01/01'), by="day"), n))
 )
 
 test_m = ChangeDetection(
@@ -16,6 +15,8 @@ test_m = ChangeDetection(
     denominator_variable = "a_denominator",
     date_variable = "timestamp"
 )
+
+context( "Input data handling")
 
 test_that( "Column names are amended correctly",
     expect_equal( sort( amend_column_names(test_m,test_df) %>% colnames ),
@@ -48,14 +49,16 @@ test_that( "Missing input file gives error", {
     test_m@name = basename( t )
     test_m@indir = dirname( t )
     write_csv( test_df, t )
-    
-    system( glue( "rm {t}" ) )
+    system( glue( "rm {t}" ) ) # Removing so that we know it doesn't exist
     
     expect_error( check_input_file_exists(test_m) )
-    
 })
 
-
+test_that( "Date to epoch calculation is correct", {
+    expect_equal( date_to_epoch(as.Date("1970-01-01")), 0 )
+    expect_equal( date_to_epoch(as.Date("1970-01-01") + seconds(10)), 10)
+    expect_equal( date_to_epoch(ymd("1970-01-01") + seconds(10)), 10)
+})
 
 
 

6062f88a59ef2c692fdd43d72864be77e2efe74b
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-09-20 10:59:49 +0100

    revert output deciles for plots

--- analysis/plot_measures.py
+++ analysis/plot_measures.py
@@ -56,7 +56,7 @@ for i in indicators_list:
                 df = df[df['age_band'].isin(['70-79', '80+'])]
         
         df = redact_small_numbers(df, 10, f"indicator_{i}_numerator", denominator, "rate")  
-        df.to_csv(OUTPUT_DIR / f'deciles_{i}.csv')
+    
         plot_measures(df = df, filename=f"plot_{i}_{d}", title=f"Indicator {i} by {d}",  column_to_plot = "rate", y_label = 'Rate per 1000', as_bar=False, category = d)
 
 # plot composite measures

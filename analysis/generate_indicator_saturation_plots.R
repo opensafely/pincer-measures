aa8dd4a8276ea5eea862abe64acaaeb84f8f0c5a
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-09-29 16:11:26 +0100

    refactor/tidy: removing cumulative break data, refactoring per-month break data, removing unnecessary plots

--- analysis/generate_indicator_saturation_plots.R
+++ analysis/generate_indicator_saturation_plots.R
@@ -43,111 +43,8 @@ if ( !dir.exists(out_dir ) ) {
 #annotation.t = 10
 
 
-
 #####################################################################
-### STEP 1: READING IN THE CUMULATIVE ANALYSIS OUTPUT AND GENERATING PLOTS
-#####################################################################
-
-pos_break_cumulative.in = read.csv( glue("{out_dir}/at-least-one_post-COVID_pos-break_cumulative.csv") )
-
-pos_break_cumulative.d_tmp = pos_break_cumulative.in %>%
-  select( -X )  %>% 
-  mutate( month = ymd( month ) ) %>%
-
-  ### Remove indicators with no counts at any time
-  group_by( indicator ) %>% 
-  mutate( nocounts = all( count == 0 ) )
-
-removed_indicators = pos_break_cumulative.d_tmp %>%
-  filter( nocounts ) %>% 
-  pull( indicator ) %>% unique
-
-pos_break_cumulative.d = pos_break_cumulative.d_tmp %>% 
-  filter( !nocounts ) %>% 
-  select( -nocounts )
-  
-xbreaks = seq( pos_break_cumulative.d %>% pull(month) %>% min(na.rm=TRUE),
-               pos_break_cumulative.d %>% pull(month) %>% max(na.rm=TRUE),
-               by='1 month' )
-
-### As line plot
-ggplot( data = pos_break_cumulative.d,
-        aes( x = month,
-             y = count,
-             group = indicator,
-             col = indicator ) ) +
-  geom_point() + 
-  geom_line() +
-  scale_x_date( date_labels = "%b %y", breaks = xbreaks ) +
-  labs( title= "Count of positive breaks (cumulative since COVID)",
-        subtitle = glue("{length(removed_indicators)} indicators \\
-                        ({paste(removed_indicators,collapse=',')}) removed due to no counts" ),
-        x = "Months relative to start of COVID (03/20)",
-        y = "Number of practices with positive break identified in this month" ) +
-  theme_bw() +
-  facet_wrap( ~indicator ) +
-  theme( axis.text.x = element_text(angle=90, hjust = 1, vjust=0.5))
-
-ggsave(glue("{out_dir}/BREAK-COUNT_POS_line-cumulative.png"), width = 12, height = 6)
-
-### As histogram
-ggplot( data = pos_break_cumulative.d,
-        aes( x = month,
-             y = count,
-             group = indicator,
-             fill = indicator,
-             col = indicator ) ) +
-  geom_bar( stat="identity") + 
-  scale_x_date( date_labels = "%b %y", breaks = xbreaks ) +
-  labs( title= "Count of positive breaks (cumulative since COVID)",
-        subtitle = glue("{length(removed_indicators)} indicators \\
-                        ({paste(removed_indicators,collapse=',')}) removed due to no counts" ),
-        x = "Months relative to 03/20",
-        y = "Number of practices with positive break identified in this month" ) +
-  theme_bw() +
-  facet_wrap( ~indicator ) +
-  theme( axis.text.x = element_text(angle=90, hjust = 1, vjust=0.5))
-
-ggsave(glue("{out_dir}/BREAK-COUNT_POS_histogram-cumulative.png"), width = 12, height = 6)
-
-### As stacked bar chart
-ggplot( data = pos_break_cumulative.d,
-        aes( x = month,
-             y = count,
-             group = indicator,
-             fill = indicator,
-             col = indicator ) ) +
-  geom_bar(  stat="identity" ) + 
-  scale_x_date( date_labels = "%b %y", breaks = xbreaks ) +
-  labs( title= "Count of positive breaks (cumulative since COVID)",
-        subtitle = glue("{length(removed_indicators)} indicators \\
-                        ({paste(removed_indicators,collapse=',')}) removed due to no counts" ),
-        x = "Months relative to 03/20",
-        y = "Number of practices with positive break identified" ) +
-  theme_bw() +
-  theme( axis.text.x = element_text(angle=90, hjust = 1, vjust=0.5))
-
-ggsave( glue("{out_dir}/BREAK-COUNT_POS_stacked-histogram-cumulative.png") )
-
-# ### As ribbon chart
-# ggplot( data = pos_break_cumulative.d,
-#         aes( x = timenum,
-#              y = count,
-#              fill = indicator) ) +
-#   geom_stream( type="ridge", colour="black" ) +
-#   scale_x_continuous( breaks = xbreaks) +
-#   labs( title= "Count of positive breaks per month",
-#         subtitle = glue("{length(removed_indicators)} indicators \\
-#                         ({paste(removed_indicators,collapse=',')}) removed due to no counts" ),
-#         x = "Months relative to 03/20",
-#         y = "Number of positive breaks identified in this month" ) +
-#   theme_bw()
-# 
-# ggsave(  glue("{out_dir}/BREAK-COUNT_ribbon.png") )
-
-
-#####################################################################
-### STEP 2: READING IN THE PER MONTH OUTPUT AND GENERATING PLOTS
+### READING IN THE PER MONTH OUTPUT AND GENERATING PLOTS
 #####################################################################
 
 ###
@@ -158,7 +55,6 @@ pos_break_permonth.in = read.csv( glue("{out_dir}/num-pos-break_permonth.csv") )
 
 pos_break_permonth.d = pos_break_permonth.in %>%
   select( -X ) %>% 
-  filter( ! indicator %in% removed_indicators ) %>% 
   mutate( month = ymd( month ) )
 
 xbreaks = seq( pos_break_permonth.d %>% pull(month) %>% min(na.rm=TRUE),
@@ -167,7 +63,7 @@ xbreaks = seq( pos_break_permonth.d %>% pull(month) %>% min(na.rm=TRUE),
 
 ggplot( data = pos_break_permonth.d,
         aes( x = month,
-             y = count,
+             y = n,
              group = indicator ) ) +
   geom_vline( xintercept = ymd("2020-03-01"),
               colour="orange",
@@ -176,8 +72,6 @@ ggplot( data = pos_break_permonth.d,
   geom_line( colour = colour_scheme["pos"] ) +
   scale_x_date( date_labels = "%b %y", breaks = xbreaks ) +
   labs( title= "Count of positive breaks per month (from the index date)",
-        subtitle = glue("{length(removed_indicators)} indicators \\
-                        ({paste(removed_indicators,collapse=',')}) removed due to no counts" ),
         x = "Months relative to index date",
         y = "Number of practices with positive break identified in this month" ) +
   theme_bw() +
@@ -194,7 +88,6 @@ neg_break_permonth.in = read.csv( glue("{out_dir}/num-neg-break_permonth.csv") )
 
 neg_break_permonth.d = neg_break_permonth.in %>% 
   select( -X ) %>% 
-  filter( ! indicator %in% removed_indicators ) %>% 
   mutate( month = ymd( month ) ) 
 
 xbreaks = seq( neg_break_permonth.d %>% pull(month) %>% min(na.rm=TRUE),
@@ -204,7 +97,7 @@ xbreaks = seq( neg_break_permonth.d %>% pull(month) %>% min(na.rm=TRUE),
 ### As line plot
 ggplot( data = neg_break_permonth.d,
         aes( x = month,
-             y = count,
+             y = n,
              group = indicator ) ) +
   geom_vline( xintercept = ymd("2020-03-01"),
               colour="orange",
@@ -213,8 +106,6 @@ ggplot( data = neg_break_permonth.d,
   geom_line( colour = colour_scheme["neg"] ) +
   scale_x_date( date_labels = "%b %y", breaks = xbreaks ) +
   labs( title= "Count of negitive breaks per month (from the index date)",
-        subtitle = glue("{length(removed_indicators)} indicators \\
-                        ({paste(removed_indicators,collapse=',')}) removed due to no counts" ),
         x = "Months relative to index date",
         y = "Number of practices with negative break identified in this month" ) +
   theme_bw() +
@@ -228,18 +119,18 @@ ggsave(glue("{out_dir}/BREAK-COUNT_NEG_line-permonth.png"), width = 12, height =
 ### 
 
 both_break_permonth.d = pos_break_permonth.d %>%
-  select( indicator, count, month ) %>% 
-  rename( pos = count ) %>% 
-  inner_join( neg_break_permonth.d %>% select( indicator, count, month ),
+  select( indicator, n, month ) %>% 
+  rename( pos = n ) %>% 
+  inner_join( neg_break_permonth.d %>% select( indicator, n, month ),
               by=c("indicator","month")) %>% 
-  rename( neg = count ) %>% 
+  rename( neg = n ) %>% 
   pivot_longer( c(pos, neg),
                 names_to = "direction",
-                values_to = "count" )
+                values_to = "n" )
 
 ggplot( data = both_break_permonth.d,
         aes( x = month,
-             y = count,
+             y = n,
              group = direction,
              col = direction ) ) +
   geom_vline( xintercept = ymd("2020-03-01"),
@@ -250,8 +141,6 @@ ggplot( data = both_break_permonth.d,
   scale_x_date( date_labels = "%b %y", breaks = xbreaks ) +
   scale_colour_manual( values=colour_scheme ) +
   labs( title= "Count of negitive breaks per month (from the index date)",
-        subtitle = glue("{length(removed_indicators)} indicators \\
-                        ({paste(removed_indicators,collapse=',')}) removed due to no counts" ),
         x = "Months relative to index date",
         y = "Number of practices with breaks identified in this month" ) +
   theme_bw( ) +

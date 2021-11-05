eb7e9466abe3dc34c73be2f40c6d9c27478c6876
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-11-05 13:35:29 +0000

    sharing attempt at egfr categorised_as() call using comparators (NB. this is not working as yet and will fail the tests)

--- analysis/study_definition_egfr.py
+++ analysis/study_definition_egfr.py
@@ -54,7 +54,7 @@ study = StudyDefinition(
     egfr_between_1_and_45 = patients.categorised_as(
         {
             "0": "DEFAULT",
-            "1": """ (egfr>=1) AND (egfr < 45)"""
+            "1": """ (egfr>=1) AND (egfr < 45) AND ( NOT egfr_comparator = '>') AND ( NOT egfr_comparator = '~') """
         },
         return_expectations = {
             "rate": "universal",

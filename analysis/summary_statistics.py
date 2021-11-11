a7aacc279381c53ae211d5d679784acf2a2d4001
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-11-11 09:35:37 +0000

    extract unique patients in denominator

--- analysis/summary_statistics.py
+++ analysis/summary_statistics.py
@@ -9,8 +9,8 @@ additional_indicators = ["e","f"]
 indicators_list.extend(additional_indicators)
 
 practice_list = []
-patient_counts_dict = {}
-patient_dict = {}
+patient_counts_dict = {"numerator": {}, "denominator": {}}
+patient_dict = {"numerator": {}, "denominator": {}}
 
 
 for file in OUTPUT_DIR.iterdir():
@@ -27,22 +27,30 @@ for file in OUTPUT_DIR.iterdir():
             if indicator in ["e", "f"]:
                 df_e_f = pd.read_feather(OUTPUT_DIR / f'indicator_e_f_{date}.feather')
                 
-                df_subset = df_e_f[df_e_f[f'indicator_{indicator}_numerator']==1]
-            
+                df_subset_numerator = df_e_f[df_e_f[f'indicator_{indicator}_numerator']==1]
+                
            
             else:
-                df_subset = df[df[f'indicator_{indicator}_numerator']==1]
+                df_subset_numerator = df[df[f'indicator_{indicator}_numerator']==1]
                 # get unique patients
             
-            
-            patients = list(df_subset['patient_id'])
+            # the same denominator is used for both mtx measures
+            if indicator in ["me_no_fbc", "me_no_lft"]:
+                df_subset_denominator = df[df[f'indicator_me_denominator']==1]
+            else:
+                df_subset_denominator = df[df[f'indicator_{indicator}_denominator']==1]
+
+            patients_numerator = list(df_subset_numerator['patient_id'])
+            patients_denominator = list(df_subset_denominator['patient_id'])
 
             if indicator not in patient_dict:
                 #create key
-                patient_dict[indicator] = patients
-            
+                patient_dict["numerator"][indicator] = patients_numerator
+                patient_dict["denominator"][indicator] = patients_denominator
+
             else:
-                patient_dict[indicator].extend(patients)
+                patient_dict["numerator"][indicator].extend(patients_numerator)
+                patient_dict["denominator"][indicator].extend(patients_denominator)
 
 
 num_practices = len(np.unique(practice_list))
@@ -52,13 +60,21 @@ with open(f'output/practice_count_{backend}.json', 'w') as f:
 
 
 
-for (key, value) in patient_dict.items():
+for (key, value) in patient_dict["numerator"].items():
     #get unique patients
-    unique_patients = len(np.unique(patient_dict[key]))
+    unique_patients_numerator = len(np.unique(patient_dict["numerator"][key]))
 
     #add to dictionary as num(mil)
-    patient_counts_dict[key] = (unique_patients)
-      
+    patient_counts_dict["numerator"][key] = (unique_patients_numerator)
+
+for (key, value) in patient_dict["denominator"].items():
+    #get unique patients
+    unique_patients_denominator = len(np.unique(patient_dict["denominator"][key]))
+
+    #add to dictionary as num(mil)
+    patient_counts_dict["denominator"][key] = (unique_patients_denominator)
+
+
 with open(f'output/patient_count_{backend}.json', 'w') as f:
     json.dump({"num_patients": patient_counts_dict}, f)
 
@@ -70,10 +86,12 @@ for indicator in indicators_list:
     df = pd.read_csv(OUTPUT_DIR / f'measure_indicator_{indicator}_rate.csv')
     num_practices, percentage_practices = get_percentage_practices(df)
     num_events = get_number_events(df, indicator)
-    num_patients = get_number_patients(indicator)
+    num_patients_numerator = get_number_patients(indicator, "numerator")
+    num_patients_denominator = get_number_patients(indicator, "denominator")
 
     counts_dict[indicator]['events'] = num_events
-    counts_dict[indicator]['patients'] = num_patients
+    counts_dict[indicator]['patients_numerator'] = num_patients_numerator
+    counts_dict[indicator]['patients_denominator'] = num_patients_denominator
     counts_dict[indicator]['num_practices'] = num_practices
     counts_dict[indicator]['percent_practice'] = percentage_practices
 

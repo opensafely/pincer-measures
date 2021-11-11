a907737d7cc9fe1113a458e93b11050edaec92a3
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-11-11 14:32:32 +0000

    add script to filter egfr

---
+++ analysis/filter_egfr.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from utilities import OUTPUT_DIR, match_input_files
+
+for file in OUTPUT_DIR.iterdir():
+    if match_input_files(file.name):
+        df = pd.read_feather(dirpath / file.name)
+        
+        
+        df_filtered = df.loc[(df['egfr'] >=1) & (df['egfr'] <45) & (~df['egfr_comparator'].isin(['>', '>=', '~'])) & ~((df['egfr'] == 1) & (df['egfr_comparator'] == '<'))]
+        df_filtered.to_feather(dirpath / file.name)

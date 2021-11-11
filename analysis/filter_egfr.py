f1f237f5f98fca742a638f912cf2c4c807c3b220
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-11-11 14:40:55 +0000

    fix: output directory path

--- analysis/filter_egfr.py
+++ analysis/filter_egfr.py
@@ -3,8 +3,8 @@ from utilities import OUTPUT_DIR, match_input_files
 
 for file in OUTPUT_DIR.iterdir():
     if match_input_files(file.name):
-        df = pd.read_feather(dirpath / file.name)
+        df = pd.read_feather(OUTPUT_DIR / file.name)
         
         
         df_filtered = df.loc[(df['egfr'] >=1) & (df['egfr'] <45) & (~df['egfr_comparator'].isin(['>', '>=', '~'])) & ~((df['egfr'] == 1) & (df['egfr_comparator'] == '<'))]
-        df_filtered.to_feather(dirpath / file.name)
+        df_filtered.to_feather(OUTPUT_DIR / file.name)

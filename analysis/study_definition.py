150250d5f0c81c60196a365891d5cbd5ed744138
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-02-17 12:38:59 +0000

    drop demographic vars

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -100,54 +100,7 @@ study = StudyDefinition(
             "category": {"ratios": {"M": 0.5, "F": 0.5}},
         }
     ),
-    msoa=patients.registered_practice_as_of(
-        "index_date",
-        returning="msoa_code",
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "E02002488": 0.1,
-                    "E02002586": 0.1,
-                    "E02002677": 0.1,
-                    "E02002814": 0.1,
-                    "E02002915": 0.1,
-                    "E02003251": 0.1,
-                    "E02000003": 0.2,
-                    "E02003334": 0.1,
-                    "E02002986": 0.1,
-                },
-            },
-        },
-    ),
-    imd=patients.categorised_as(
-        {
-            "0": "DEFAULT",
-            "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
-            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
-            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
-            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
-            "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation < 32844""",
-        },
-        index_of_multiple_deprivation=patients.address_as_of(
-            "index_date",
-            returning="index_of_multiple_deprivation",
-            round_to_nearest=100,
-        ),
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "0": 0.05,
-                    "1": 0.19,
-                    "2": 0.19,
-                    "3": 0.19,
-                    "4": 0.19,
-                    "5": 0.19,
-                }
-            },
-        },
-    ),
+    
     ###
     # CO-PRESCRIBING-VARS
     ###

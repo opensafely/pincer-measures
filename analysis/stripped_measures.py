e1a9acfa19144c826034649b5ce0518f055046e3
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-02-24 15:40:09 +0000

    include indicators e and f in stripped measures

--- analysis/stripped_measures.py
+++ analysis/stripped_measures.py
@@ -3,6 +3,9 @@ import pandas as pd
 from config import indicators_list
 from utilities import OUTPUT_DIR, produce_stripped_measures
 
+additional_indicators = ["e", "f"]
+indicators_list.extend(additional_indicators)
+
 for indicator in indicators_list:
 
     df = pd.read_csv(OUTPUT_DIR / f'measure_indicator_{indicator}_rate.csv')

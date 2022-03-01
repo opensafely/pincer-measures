dbf50758ed8b2502fc9299303f892fb59f1d769b
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-03-01 15:46:35 +0000

    update region count

--- analysis/check_region.py
+++ analysis/check_region.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from utilities import OUTPUT_DIR, ANALYSIS_DIR,match_input_files_region
+from utilities import OUTPUT_DIR, ANALYSIS_DIR,match_input_files_region, update_demographics
 
 msoa_to_region = pd.read_csv(
         ANALYSIS_DIR / "ONS_MSOA_to_region_map.csv",
@@ -12,18 +12,22 @@ msoa_to_region = pd.read_csv(
     
 msoa_dict = dict(zip(msoa_to_region["MSOA11CD"], msoa_to_region["RGN11NM"]))
 
+
+demographics_df = pd.DataFrame(columns=["patient_id", "region", "region_msoa"])
+
+
 counts_list = []
 for file in OUTPUT_DIR.iterdir():
     if match_input_files_region(file.name):
         df = pd.read_csv(OUTPUT_DIR / file.name)
 
         df["region_msoa"] = df["msoa"].map(msoa_dict)
-        similar = df.groupby(['region','region_msoa']).size().reset_index()
-        counts_list.append(similar)
+        demographics_df = update_demographics(demographics_df, df)
 
-combined = pd.concat(counts_list)
-combined = combined.groupby(by=['region', 'region_msoa']).sum()
-combined.to_csv(OUTPUT_DIR / 'combined_count.csv')
+
+
+similar = demographics_df.groupby(['region','region_msoa']).size().reset_index()
+similar.to_csv(OUTPUT_DIR / 'combined_count.csv')
 
       
         

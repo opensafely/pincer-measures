f5c2832ce7564460ff7201a14533a275fbac3ba1
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-03-07 17:04:28 +0000

    use latest month for demographic summary

--- analysis/demographics_summary.py
+++ analysis/demographics_summary.py
@@ -27,25 +27,47 @@ for file in OUTPUT_DIR.iterdir():
 
         date = get_date_input_file(file.name)
 
-        dem_df = pd.read_csv(OUTPUT_DIR / f"input_demographics_{date}.csv.gz")
-        region_df = pd.read_csv(OUTPUT_DIR / f"input_region_{date}.csv.gz")
-
-        for d in demographics:
-
-            if d == "ethnicity":
-                demographics_dict = dict(
-                    zip(ethnicity_df["patient_id"], ethnicity_df[d])
-                )
-
-            elif d == "region":
-                demographics_dict = dict(zip(region_df["patient_id"], region_df[d]))
+        if date == "2021-09-01":
+          
+            dem_df = pd.read_csv(OUTPUT_DIR / f"input_demographics_{date}.csv.gz")
+            region_df = pd.read_csv(OUTPUT_DIR / f"input_region_{date}.csv.gz")
             
-            else:
-                demographics_dict = dict(zip(dem_df["patient_id"], dem_df[d]))
+            for d in demographics:
 
-            df[d] = df["patient_id"].map(demographics_dict)
+                if d == "ethnicity":
+                    demographics_dict = dict(
+                        zip(ethnicity_df["patient_id"], ethnicity_df[d])
+                    )
 
-        demographics_df = update_demographics(demographics_df, df)
+                elif d == "region":
+                    demographics_dict = dict(zip(region_df["patient_id"], region_df[d]))
+                
+                else:
+                    demographics_dict = dict(zip(dem_df["patient_id"], dem_df[d]))
+
+                df[d] = df["patient_id"].map(demographics_dict)
+            
+            ethnicity_mapping = {
+                "1": "1",
+                "2": "1",
+                "3": "1",
+                "4": "2",
+                "5": "2",
+                "6": "2",
+                "7": "2",
+                "8": "3",
+                "9": "3", 
+                "10": "3",
+                "11": "3",
+                "12": "4",
+                "13": "4",
+                "14": "4",
+                "15": "5",
+                "16": "5"
+            }
+            df["ethnicity"] = df["ethnicity"].map(ethnicity_mapping)
+            
+            demographics_df = update_demographics(demographics_df, df)
 
     d_list = {}
     for d in demographics:

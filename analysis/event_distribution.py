987c75ebe141da511b9427ccf6bff4ef0eee7b1d
Louis Fisher <louis.fisher1919@gmail.com>
2023-06-21 10:13:17 +0100

    update bins

--- analysis/event_distribution.py
+++ analysis/event_distribution.py
@@ -14,8 +14,17 @@ for indicator in indicators_list:
 
     df_subset = df.loc[(df["rate"]>0)&(df["date"]=="2020-01-01"),:]
     
-    data_cut = pd.cut(df_subset[f"indicator_{indicator}_numerator"], bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,float("inf")])
+    data_cut = pd.cut(df_subset[f"indicator_{indicator}_numerator"], bins=[0, 6, 7, 8, 9, 10,float("inf")])
     data_cut = data_cut.value_counts().sort_index()
+
+    # replace "(0.0, 6.0]" with "<=6"
+    data_cut.index = data_cut.index.astype(str)
+    data_cut.index = data_cut.index.str.replace("(0.0, 6.0]", "<=5")
+    data_cut.index = data_cut.index.str.replace("(6.0, 7.0]", "6")
+    data_cut.index = data_cut.index.str.replace("(7.0, 8.0]", "7")
+    data_cut.index = data_cut.index.str.replace("(8.0, 9.0]", "8")
+    data_cut.index = data_cut.index.str.replace("(9.0, 10.0]", "9")
+    data_cut.index = data_cut.index.str.replace("(10.0, inf]", "10+")
    
     data_cut.name = "count"
     data_cut = data_cut.apply(lambda x: round(x / 5) * 5)

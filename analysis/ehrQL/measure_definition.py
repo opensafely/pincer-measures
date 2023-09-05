bd7048a8b91f46a1fad5f351d5405b4d24d2f529
Louis Fisher <louis.fisher1919@gmail.com>
2023-09-05 09:35:58 +0100

    reinstate full time period

--- analysis/ehrQL/measure_definition.py
+++ analysis/ehrQL/measure_definition.py
@@ -295,8 +295,8 @@ indicator_am_numerator = (
 
 ### Measures
 start_date = "2019-01-01"
-# num_intervals = calculate_num_intervals(start_date)
-num_intervals=1
+num_intervals = calculate_num_intervals(start_date)
+
 
 
 # Initialize the measures
